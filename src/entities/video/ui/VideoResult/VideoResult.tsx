1193bf0fc1ac88d578f0293ed2dd8ba017cbce6f
Anton Zhuravlev <abramoff.job@gmail.com>
2024-10-13 08:39:53 +0300

    123

--- src/entities/video/ui/VideoResult/VideoResult.tsx
+++ src/entities/video/ui/VideoResult/VideoResult.tsx
@@ -1,40 +1,40 @@
 import cls from './VideoResult.module.scss';
 import { useParams } from 'react-router-dom';
-import { useLazyGetStatusQuery } from '@entities/video';
 import { useEffect, useState } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css'; // Стили для скелетонов
 
 export const VideoResult = () => {
     const { id } = useParams<{ id: string }>(); // Получаем параметр id из URL
-    const [trigger, { data }] = useLazyGetStatusQuery();
+    const [videoState, setVideoState] = useState<string | null>(null);
+    const [imageSrc, setImageSrc] = useState<string | null>(null);
+    const [socket, setSocket] = useState<WebSocket | null>(null);
+
     useEffect(() => {
         if (id) {
-            const interval = setInterval(() => {
-                trigger({ id });
-            }, 2000);
-            if (data?.Video.state === 'SUCCESSFUL') {
-                clearInterval(interval);
-            }
+            const ws = new WebSocket(`${import.meta.env.VITE_WS_SERVER_URL}/img`);
+            setSocket(ws);
+
+            ws.onmessage = (event) => {
+                console.log(event);
+            };
+
+            // Очистка WebSocket при размонтировании компонента
+            return () => {
+                ws.close();
+            };
         }
-    }, [id, trigger]);
+    }, [id]);
 
     return (
         <div className={cls.wrapper}>
             <div className={cls.status}>
-                <p className={cls.text}>Статус видео: {data?.Video.state}</p>
+                <p className={cls.text}>Статус видео: {videoState}</p>
             </div>
-            {data?.Video.state === 'SUCCESSFUL' ? (
-                <div className={cls.videoWrapper}>
-                    <video
-                        controls={true}
-                        autoPlay={true}
-                        className={cls.video}
-                        src={`${import.meta.env.VITE_SERVER_URL}/minio/localbucket/output_video.mp4`}
-                    />
-                    <a href={`${import.meta.env.VITE_SERVER_URL}/minio/localbucket/output_video.mp4`}>
-                        <button>Скачать</button>
-                    </a>
+
+            {imageSrc ? (
+                <div className={cls.imageWrapper}>
+                    <img src={imageSrc} alt="Превью видео" className={cls.image} />
                 </div>
             ) : (
                 <Skeleton className={cls.skeleton} />
